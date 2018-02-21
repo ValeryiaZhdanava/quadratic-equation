bcbd7e4f47bbfe576fe1b8dde6f95a97f7dc4ae2
Valeryia Zhdanava <vzhd@ciklum.com>
2018-02-21 23:56:31 +0300

    add solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,23 @@
 module.exports = function solveEquation(equation) {
-  // your implementation
+    let el = equation.split(' ');
+    let a = el[0];
+    let b = el[3] + el[4];
+    let c = el[7] + el[8];
+
+    let d = Math.pow(b, 2) - (4 * a * c);
+
+    let result = [];
+
+    if (d > 0) {
+        let res1 = Math.round((-b + Math.sqrt(d)) / (2 * a));
+        let res2 = Math.round((-b - Math.sqrt(d)) / (2 * a));
+        result.push(res1, res2);
+    }
+
+    result.sort((a, b) => {
+        return (a > b) ? 1 : -1;
+    });
+
+    return result;
+
 }
